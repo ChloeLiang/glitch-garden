08ea61b56abfd60c2d9ea559c03247cd4c679627
ChloeLiang <nliangxin@gmail.com>
2017-12-25 10:30:52 +0800

    Add improvement - Load volume setting at Start - Change fade in script to animation - Prefab attackers and defenders animations

---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetStartVolume : MonoBehaviour {
+	private MusicManager musicManager;
+
+	void Start () {
+		musicManager = GameObject.FindObjectOfType<MusicManager>();
+		if (musicManager) {
+			float volume = PlayerPrefsManager.GetMasterVolume();
+			musicManager.SetVolume(volume);
+		} else {
+			Debug.LogWarning("No music manager found in Start scene, can't set volume");
+		}
+	}
+}
