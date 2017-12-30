0fd3fccac1d71c6b79049cd93c6494fda2bfd34b
ChloeLiang <nliangxin@gmail.com>
2017-12-30 11:58:07 +0800

    Add health component

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+	public float health = 100f;
+
+	public void DealDamange(float damage) {
+		health -= damage;
+		if (health < 0) {
+			// Optionally trigger animation
+			DestroyObject();
+		}
+	}
+
+	// Call death animation in Animator Event
+	public void DestroyObject() {
+		Destroy(gameObject);
+	}
+}
