6c96dd9035f50a55eb96977cbd22043a40690fec
ChloeLiang <nliangxin@gmail.com>
2018-01-07 17:53:05 +0800

    Spawn defenders to grid

--- Assets/Scripts/Attacker.cs
+++ Assets/Scripts/Attacker.cs
@@ -19,7 +19,6 @@ public class Attacker : MonoBehaviour {
 		if (!currentTarget) {
 			animator.SetBool("isAttacking", false);
 		}
-		print(Button.selectedDefender);
 	}
 
 	void OnTriggerEnter2D () {
